f78c6ad833d5acacae0ffeea188de3d344f17f64
lnobreteixeira-git <71680589+lnobreteixeira-git@users.noreply.github.com>
2021-09-01 11:31:31 -0300

    Update boot.ps1

--- boot.ps1
+++ boot.ps1
@@ -1,38 +1,18 @@
 $server = Get-Content servers.txt
 
- 
-
 foreach($server in $server){
 
- 
-
-$service = Get-Service -ComputerName $server |Where-Object {$_.Status -eq "Running"}
-
- 
+ $service = Get-Service -ComputerName $server |Where-Object {$_.Status -eq "Running"}
 
 Restart-Computer -ComputerName $server -Force
 
- 
-
 Write-Host "Servidor $server reiniciando ..."
 
- 
-
 # Aguardar o tempo de 60 segundos para que o serviço de RDP baixe ou baixar o serviço antes de reiniciar a máquina ai sim pode-se retirar o sleep 60 abaixo:
-
- 
-
 sleep 90
 
- 
-
 #testando a conexao depois do boot
-
- 
-
 $conecta = Test-Connection -ComputerName $server -Count 1 -Quiet
-
- 
 
     if (($conecta) -ne "True" ){
 
@@ -53,24 +33,16 @@ $conecta = Test-Connection -ComputerName $server -Count 1 -Quiet
             }
 
          while ( ($rdp).TCPTestSucceeded -ne "True" )
-
-        
-
+   
         Write-Host "O $server JA REINICIOU, VAMOS VALIDAR OS SERVICOS"
-
-        
-
+   
         # o sleep abaixo é obrigatório para garantir que os serviços já subiram
 
         sleep 30
 
- 
-
         foreach ($service in $service){
 
         sleep 1
-
- 
 
         $status = Get-Service $service.Name -ComputerName $server
 
@@ -86,8 +58,6 @@ $conecta = Test-Connection -ComputerName $server -Count 1 -Quiet
 
                  }
 
- 
-
              catch {
 
                     Write-Host "Ocorreu erro para subir o servico" $service.Name
@@ -98,11 +68,8 @@ $conecta = Test-Connection -ComputerName $server -Count 1 -Quiet
 
   }  # fechamento de laco do foreach service in service
 
- 
-
   }  # senao do if conect ne True
 
   Write-Host "BOOT REALIZADO COM SUCESSO NO SERVIDOR $server"
 
 }  # fechamento de laco do foreach server in server
- 
